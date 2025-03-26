ba8298aecc426931af5da047ee05b07ba8ded237
Iyanuloluwa Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-03-26 10:43:28 -0400

    Update Wine Analysis.py

--- Wine Analysis.py
+++ Wine Analysis.py
@@ -230,7 +230,7 @@ X_train, X_test, y_train, y_test = train_test_split(X,y , random_state=104,test_
 """
 
 # Define KNN classifier
-model = KNeighborsRegressor(n_neigbours=5)
+model = KNeighborsRegressor(n_neighbors=5)
 
 model.fit(X_train,y_train)
 
